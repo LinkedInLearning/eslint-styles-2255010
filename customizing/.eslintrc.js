cb0df88125047a224b465d2b115af2c05cc17dbe
JamesVanWaza <jgacuca@gmail.com>
2021-02-08 07:02:13 -0500

    Updated the comma-spacing rule

--- customizing/.eslintrc.js
+++ customizing/.eslintrc.js
@@ -1,30 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: 'eslint:recommended',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    'brace-style': 'warn',
-    'no-unused-vars': 'off',
-    'object-curly-spacing': [
-      'error',
-      'always',
-    ],
-    'comma-spacing': [
-      'error',
-      {
-        'before': 'true',
-        'after': 'true',
-      },
-    ],
-  },
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        'brace-style': 'warn',
+        'no-unused-vars': 'off',
+        'object-curly-spacing': [
+            'error',
+            'always',
+        ],
+        'comma-spacing': [
+            "error", {
+                "before": false,
+                "after": true
+            }
+        ]
+    },
 };
